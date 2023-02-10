797447ec0dd7a9a638543caff8b799495642ad98
Joao Vitor Brito <joaovbrito123@gmail.com>
2023-02-10 09:30:35 -0300

    Creating project and navbar

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script></script>
+
+<template>
+  <nav class="navbar navbar-expand-lg ">
+    <div class="container-fluid d-flex align-items-center">
+      <router-link class="navbar-brand text-white d-flex align-items-center gap-2" to="/">
+        <img src="@/assets/pokeball.svg" alt="" class="img-fluid">
+        Pokedex
+      </router-link>
+      <button class="navbar-toggler border-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon" style="color:white"></span>
+      </button>
+      <div class="collapse navbar-collapse text-center " id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link text-white" aria-current="page" to="/">Ínicio</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link text-white" to="/about">Sobre</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="main">
+    <router-view></router-view>
+  </div>
+
+</template>
+
+<style>
+  body {
+    background-color: #C23D3D;
+    font-family: 'Inter';
+  }
+</style>
