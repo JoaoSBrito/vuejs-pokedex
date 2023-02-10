469bee1ff946237b3da66875112f9dbb95fc4533
Joao Vitor Brito <joaovbrito123@gmail.com>
2023-02-10 11:28:22 -0300

    List pokemon

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script></script>
 
 <template>
-  <nav class="navbar navbar-expand-lg ">
+  <nav class="navbar navbar-expand-lg">
     <div class="container-fluid d-flex align-items-center">
       <router-link class="navbar-brand text-white d-flex align-items-center gap-2" to="/">
         <img src="@/assets/pokeball.svg" alt="" class="img-fluid">
@@ -10,7 +10,7 @@
       <button class="navbar-toggler border-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon" style="color:white"></span>
       </button>
-      <div class="collapse navbar-collapse text-center " id="navbarNav">
+      <div class="collapse navbar-collapse text-center" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
             <router-link class="nav-link text-white" aria-current="page" to="/">Ínicio</router-link>
@@ -27,6 +27,12 @@
     <router-view></router-view>
   </div>
 
+  <footer>
+    <div class="text-center text-white">
+      Criado por <a class="text-white text-bold fw-bold " href="https://www.linkedin.com/in/joao-vitor-brito/" target="_blank">João Vitor Brito</a> em {{ new Date().getFullYear() }}
+    </div>
+  </footer>
+
 </template>
 
 <style>
@@ -34,4 +40,19 @@
     background-color: #C23D3D;
     font-family: 'Inter';
   }
+
+  footer {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 3rem;
+  }
+
+  footer a {
+    font-family: 'Roboto Serif', sans-serif;
+  }
+
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,34 @@
 <script setup>
+  import { onMounted, reactive, ref } from 'vue'
+  import PokemonList from '../components/PokemonList.vue';
+
+  let pokemons = reactive(ref())
+
+  onMounted(() => {
+      fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+      .then(res => res.json())
+      .then(res => pokemons.value = res.results)
+  })
 </script>
 
 <template>
-  <main>
-    Home
-  </main>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 col-md-6">
+        <div class="card">
+          <PokemonList
+            v-for="pokemon in pokemons"
+            :key="pokemon.name"
+            :name="pokemon.name"
+          />
+          <!-- <ul>
+            <li 
+              v-for="pokemon in pokemons"
+              :key="pokemon.name"
+            >{{pokemon.name}}</li>
+          </ul> -->
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
