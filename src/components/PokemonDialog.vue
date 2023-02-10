42c571104da7ac0f31e6b17e685cb0b0364af7c6
Joao Vitor Brito <joaovbrito123@gmail.com>
2023-02-10 16:30:24 -0300

    adding dialog

---
+++ src/components/PokemonDialog.vue
@@ -0,0 +1,13 @@
+<script setup>
+const pokemon = defineProps(["name"])
+</script>
+
+
+<template>
+  <h1>{{pokemon.name}}</h1>
+  
+</template>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,36 +1,72 @@
 <script setup>
-  import { onMounted, reactive, ref } from 'vue'
+  import { onMounted, reactive, ref, computed } from 'vue'
   import PokemonList from '../components/PokemonList.vue';
+  import PokemonDialog from '../components/PokemonDialog.vue';
 
   let pokemons = reactive(ref())
   let urlBaseSvg = ref("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/")
+  let searchForPokemon = ref("")
+  let selectedPokemon = reactive(ref())
 
   onMounted(() => {
       fetch("https://pokeapi.co/api/v2/pokemon?limit=400&offset=0")
       .then(res => res.json())
       .then(res => pokemons.value = res.results)
   })
+
+  const filteredPokemons = computed(() => {
+    if(pokemons.value && searchForPokemon.value) {
+      return pokemons.value.filter(pokemon => pokemon.name.toLowerCase().includes(searchForPokemon.value.toLowerCase()))
+    }
+    return pokemons.value
+  })
+
+  const selectPokemon = async (pokemon) => {
+    await fetch(pokemon.url)
+      .then(res => res.json())
+      .then(res => selectedPokemon.value = res)
+
+      console.log(selectedPokemon.value)
+  }
+
 </script>
 
 <template>
   <div class="container">
-    <div class="logo">
-      <img src="@/assets/logo.png" alt="">
+
+    <PokemonDialog 
+      :name="selectedPokemon?.name"
+    />
+
+
+    <div class="mb-3">
+      <label 
+        hidden 
+        for="searchPokemon" 
+        class="form-label">
+        Digite o nome do Pokemon...
+      </label>
+
+      <input 
+        v-model="searchForPokemon"
+        type="text" 
+        class="form-control" 
+        id="searchPokemon" 
+        placeholder="Digite o nome do Pokemon..."
+        />
     </div>
     <div class="row">
       <div class="col-sm-12 col-md-6">
-        <div class="card">
+        <div class="card card-list">
           <div class="card-body row">
 
-            <div class="mb-3">
-              <label for="exampleFormControlInput1" class="form-label">Email address</label>
-              <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-            </div>
           <PokemonList
-            v-for="pokemon in pokemons"
+            v-for="pokemon in filteredPokemons"
             :key="pokemon.name"
             :name="pokemon.name"
+            :alt="pokemon.name"
             :urlBaseSvg="urlBaseSvg + pokemon.url.split('/')[6] + '.svg'"
+            @click="selectPokemon(pokemon)"
           />
         </div>
           <!-- <ul>
@@ -44,3 +80,11 @@
     </div>
   </div>
 </template>
+
+<style scoped>
+  .card-list {
+    overflow-x: hidden;
+    overflow-y: scroll;
+    max-height: 350px;
+  }
+</style>
